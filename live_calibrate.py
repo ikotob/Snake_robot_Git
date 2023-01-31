d578a6f4d0a59788706d444bd696d7f0565579b1
ikotob <isaackotob@gmail.com>
2023-01-31 00:11:39 -0500

    Initial commit

---
+++ live_calibrate.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Load the array from a .npy file
+intrinsic_params = np.load("intrinsic_params.npy")
+distortion_params = np.load("distortion_params.npy")
+rotational_params = np.load("rotational_params.npy")
+translational_params = np.load("translational_params.npy")
+
+print(intrinsic_params)
+print(distortion_params)
