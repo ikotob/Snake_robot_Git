75f33fcaf93594eed7db67b0df040dd4311a14a2
ikotob <isaackotob@gmail.com>
2023-01-31 15:01:34 -0500

    Updated live_calibrate.py to capture live video

--- live_calibrate.py
+++ live_calibrate.py
@@ -1,13 +1,27 @@
 import numpy as np
+import cv2
 
-# Load the array from a .npy file
-intrinsic_params = np.load("intrinsic_params.npy")
-distortion_params = np.load("distortion_params.npy")
-rotational_params = np.load("rotational_params.npy")
-translational_params = np.load("translational_params.npy")
+# Load intrinsic and distortion parameters from .npy files
+cameraMatrix = np.load("intrinsic_params.npy")
+distCoeffs = np.load("distortion_params.npy")
 
-print(intrinsic_params)
-print(distortion_params)
+# Start video capture
+cap = cv2.VideoCapture(0)
 
-# HELLOAOFOISRJFIOWJFEWFEFD
-# test
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    # Undistort the frame using the stored parameters
+    undistorted_frame = cv2.undistort(frame.copy(), cameraMatrix, distCoeffs)
+
+    # Show the undistorted frameq
+    cv2.imshow("Undistorted frame", undistorted_frame)
+
+    # Break the loop if the user presses the 'q' key
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the video capture and destroy the windows
+cap.release()
+cv2.destroyAllWindows()
